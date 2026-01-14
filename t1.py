6b1ae94fa2a85bad40f2de5fa4470d814cc39cb0
Yali-Cohen <yhlyk8498@gmail.com>
2026-01-14 12:55:35 +0200

    Add mouse changes

--- t1.py
+++ t1.py
@@ -1,5 +1,10 @@
+#client, controlled
 from client1 import Client
+from input_controller import InputController
+def handle_mouse():
+    inputController = InputController()
 c = Client()
-c.connect(("192.168.1.228", 1234))
+c.connect("192.168.1.228", 1234)
 print(c.receive())
 c.send("Hello from client".encode())
+handle_mouse()
--- t2.py
+++ t2.py
@@ -1,6 +1,10 @@
 #server.py
 from server1 import Server
+from input_capture import InputCapture
+def handle_mouse_server():
+    inputCapture = InputCapture()
 s = Server(port=0)
 client_socket = s.accept_connection()
 client_socket.send(b"Hello from server")
 print(client_socket.recv(1024).decode())
+handle_mouse_server()
